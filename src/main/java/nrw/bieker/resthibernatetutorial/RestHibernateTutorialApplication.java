44c6cff826171e277a3ca5870d5971c88f9ba25f
jb <jb@jb-VirtualBox>
2018-12-28 08:02:01 +0100

    Initial Commit

---
+++ src/main/java/nrw/bieker/resthibernatetutorial/RestHibernateTutorialApplication.java
@@ -0,0 +1,14 @@
+package nrw.bieker.resthibernatetutorial;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestHibernateTutorialApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestHibernateTutorialApplication.class, args);
+	}
+
+}
+
---
+++ src/test/java/nrw/bieker/resthibernatetutorial/RestHibernateTutorialApplicationTests.java
@@ -0,0 +1,17 @@
+package nrw.bieker.resthibernatetutorial;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class RestHibernateTutorialApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
+
